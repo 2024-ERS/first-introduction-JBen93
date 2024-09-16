5d1550b2e46eedc1f280933359e55846d9623fa5
JBen93 <joshua.benjamin@ufl.edu>
2024-09-16 12:41:48 +0200

    script worked

--- r-scripts/08_regression_modelling.R
+++ r-scripts/08_regression_modelling.R
@@ -4,94 +4,63 @@
 # clear everything in memory (of R)
 remove(list=ls())
 
-renv::restore()
 library(tidyverse)
 
 
 # read the macrodetritivore abundance data, call the dataset orchdat
 # filter to use only years 2018,2019,2021,2022,2023,2024 and only 3 or less replicates and species Orchestia_gammarellus
-# group by year and TransectPoint_ID
+# group by year and Distance_ID
 # calculate the sum of the number of Orchestia found per year and TransectPoint
 # do all the above in one pipeline
-#only use 3 or less  replicates
-orchdat<-readr::read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vT6bdROUwchBBIreCQQdBenu35D5Heo8bl3UgES9wrpBwax_GUM1bKSo_QXfmLq8Ml9-crCI7MmW2XH/pub?gid=615066255&single=true&output=csv") |>
-  dplyr::filter(year %in% c(2018,2019,2021,2022,2023,2024),
-                replicate <= 3,
-                Species_ID == "Orchestia_gammarellus") |>
-  dplyr::group_by(year, TransectPoint_ID) |>
-  dplyr::summarise(CountSum=sum(Count, na.rm = T))
-print(orchdat)
+orchdat<-readr::read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vT6bdROUwchBBIreCQQdBenu35D5Heo8bl3UgES9wrpBwax_GUM1bKSo_QXfmLq8Ml9-crCI7MmW2XH/pub?gid=615066255&single=true&output=csv") 
 
 
 # read the macrotransect elevation data, filter and select right variables
 elevdat<-readr::read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vT4C7olgh28MHskOjCIQGYlY8v5z1sxza9XaCccwITnjoafF_1Ntfyl1g7ngQt4slnQlseWT6Fk3naB/pub?gid=1550309563&single=true&output=csv") %>%
   dplyr::filter(year %in% c(2018,2019,2021,2022,2023,2024) & !is.na(TransectPoint_ID)) %>%
-  dplyr::select(year,TransectPoint_ID,elevation_m)   # select  only distance_m and elevation
+  dplyr::select(year,TransectPoint_ID,elevation_m)   # select  only distance_m and elevation 
 elevdat
 
 # join  the elevation with  the orchestia data by year and TransectPoint_ID, and filter to retain only transect points between 200 and 1000 (where it lives)
 # make year a factor
-orchdat3<-left_join(orchdat, elevdat, by=c("year", "TransectPoint_ID")) |>
-  dplyr::filter(TransectPoint_ID>=200 & TransectPoint_ID <=1000) |>
-  dplyr::filter(!(year==2022 & TransectPoint_ID==260))
-
-print(orchdat3)
+orchdat3<-left_join()
 
 # explore how Orchestia abundance changes along the transect in a bar plot
-orchdat3 |>
-  ggplot2::ggplot(mapping=aes(x=factor(TransectPoint_ID), y=CountSum, group = year)) +
-  geom_bar(stat="identity") + # making a bar plot
-  facet_grid(year~.) # ~ means: what is in rows vs. (~) what is in columns
 
 
 # explore how elevation changes along the transect in a barplot
 
 
 # plot Orchestia (y) versus elevation_m (x) in ggplot as a scatterplot, with each year as a different color
-p1<- orchdat3  |>
-  ggplot2::ggplot(mapping=aes(x=elevation_m,y=CountSum, col = factor(year))) +
-  geom_point(size=3)
-print(p1)
-
-# calculate the optimal preferred elevation by Orchestia for each year using weighted.mean function
-orchdat3 |>
+p1<- orchdat3  |> 
+  
+  # calculate the optimal preferred elevation by Orchestia for each year using weighted.mean function
+  orchdat3 |>
   
   ## explore response to elevation and year as a linear model, call this m1
   # first only elevation (not yet year)
   orchdat3
 m1<-lm()
 
-#first only elevation (not year)
-orchdat3
-m1<-lm(CountSum~elevation_m, data=orchdat3)
-print(m1)
-
 #add the linear model to the plot
-orchdat3$pred1<-predict(m1)
-p2<-p1 + geom_line(data=orchdat3, aes(y=pred1),col="black",linewidth=1.2)
-print(p2)
+
+
 # fitmodel  m2 by adding a quadratic term for elevation_m to check for an ecological optimum
-# y=b0 + b1x1 + b2*I(x^2) #expect the b2 is negative
-m2<-lm(CountSum~elevation_m+I(elevation_m^2), data=orchdat3)
-print(m2)
+# y=b0 + b1x1 + b2x1^2
 
 #add the linear model to the plot
 # calculate the predicted value of m2 for every observation, add to the dataset as a variable as pred2
 # add the new predicted line to the previous plot p2, store as object p3 and show it
-orchdat3$pred2<-predict(m2)
-p2+geom_line(data=orchdat3, aes(y=pred2),col="red",linewidth=1.2)
+
 
 # test if the new model m2   significantly explains more variation than the first model m1
-anova(m1,m2)
-
 
 
 # predict the orchestia abundance at 1.5 m elevation
 
 
-
 # add year (as a factor) to the model and fit it as model m3
-# so y = b0 + b1x1 + b2x1^2 +b3x2
+# so y = b0 + b1x1 + b2x1^2 +b3x2 
 # test if it is significant, and better than the previous one
 
 
@@ -105,7 +74,7 @@ anova(m1,m2)
 
 # yes this is a better model
 
-# include the interaction between elevation and year (as a factor) in the model
+# include the interaction between elevation and year (as a factor) in the model 
 
 
 #add the linear model to the plot
@@ -138,7 +107,7 @@ log(1)
 exp(1)
 log(exp(1))
 
-### develop, test for significance and plot different models of increasing complexity
+### develop, test for significance and plot different models of increasing complexity 
 #  using  multiple regresssion, assuming a poisson distribution (so use a generalized linear model)
 # Explore  how the abundance of Orchestia depends on elevation_m and year,  their potential interaction,
 # and a potential ecological optimum of Orchestia with respect to elevation_m
@@ -169,4 +138,8 @@ log(exp(1))
 #add the  model to the plot
 # calculate the predicted value of m2 for every observation, add to the dataset as a variable as pred2
 # add the new predicted line to the previous plot p2, store as object p3 and show it
+
+
+
+
 
